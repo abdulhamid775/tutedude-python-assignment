ad87345ea562a840ab137941d88455a589ca6926
abdulhamid775 <abdulhamid1106s@gmail.com>
2025-07-13 01:12:02 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,11 @@
+# greeting.py
+
+# Taking input from the user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating full name
+full_name = first_name + " " + last_name
+
+# Displaying personalized message
+print(f"\nHello, {full_name}! Welcome to the Python program.")
